34482a8d5feee4d6b2da7b10769b35bd2bc21aa8
csy-59 <csy010509@naver.com>
2023-01-04 22:57:13 +0900

    [Feat] 기본 마우스, 키 입력 완료
    - 마우스로 특정 타일 클릭 가능
    - 키 입력으로 선택하는 타일 변경 가능

---
+++ My project/Assets/Scripts/MapToolSelecter.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapToolSelecter : MonoBehaviour
+{
+
+    [SerializeField] private Camera _mainCamera;
+    [SerializeField] private float _maxDistance = 3000f;
+    [SerializeField] private LayerMask _clickLayer;
+
+    public enum ETileType
+    {
+        Basic,
+        Bush,
+        River,
+        Wall,
+        Max,
+    }
+    private const int _keyCodeAlphaStart = (int) KeyCode.Alpha1;
+    private const int _keyCodeKeypadStart = (int) KeyCode.Keypad1;
+    private const int _keyCodeLenth = (int)ETileType.Max;
+
+    private ETileType _currentTileType = ETileType.Basic;
+
+    private void Awake()
+    {
+
+    }
+
+    private void Update()
+    {
+        GetCurrentTileType();
+        GetMouseClickPosition();
+    }
+
+    private void GetCurrentTileType()
+    {
+        for (int i = 0; i < _keyCodeLenth; ++i)
+        {
+            if (Input.GetKeyDown((KeyCode)(_keyCodeAlphaStart + i)) ||
+                Input.GetKeyDown((KeyCode)(_keyCodeKeypadStart + i)))
+            {
+                _currentTileType = (ETileType)i;
+                break;
+            }
+        }
+    }
+
+    private void GetMouseClickPosition()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            Ray ray = _mainCamera.ScreenPointToRay(Input.mousePosition);
+
+            RaycastHit hit;
+            if(Physics.Raycast(ray, out hit, _maxDistance ,_clickLayer))
+            {
+                Debug.Log($"{hit.collider.gameObject.name} {_currentTileType}");
+            }
+        }
+    }
+}
