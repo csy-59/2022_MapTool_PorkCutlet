5fe0bc980b7e25703bd01edb117d166c484e051d
csy-59 <csy010509@naver.com>
2023-01-07 12:31:20 +0900

    [Fix] 플레이어 이동 로직 수정

--- My project/Assets/Scripts/Player/PlayerMovement.cs
+++ My project/Assets/Scripts/Player/PlayerMovement.cs
@@ -10,8 +10,10 @@ public class PlayerMovement : MonoBehaviour
     public float MaxMoveSpeed { get; private set; }
     public float MoveSpeed { get => _moveSpeed; set => _moveSpeed = value; }
 
-    private readonly string _horizontal = "Horizontal";
-    private readonly string _vertical = "Vertical";
+    private const string _horizontal = "Horizontal";
+    private const string _vertical = "Vertical";
+    private float _horizontalInput;
+    private float _verticalInput;
 
     private void Awake()
     {
@@ -20,11 +22,17 @@ public class PlayerMovement : MonoBehaviour
         MoveSpeed = MaxMoveSpeed;
     }
 
-    private void Update()
+    private void FixedUpdate()
     {
-        Vector3 offsetPosition = new Vector3(Input.GetAxisRaw(_horizontal), 0f, Input.GetAxisRaw(_vertical)).normalized;
+        Vector3 offsetPosition = new Vector3(_horizontalInput, 0f, _verticalInput).normalized;
         Vector3 nextPosition = MoveSpeed * Time.deltaTime * offsetPosition + _rigidBody.position;
 
         _rigidBody.MovePosition(nextPosition);
+    }
+
+    private void Update()
+    {
+        _horizontalInput = Input.GetAxisRaw(_horizontal);
+        _verticalInput = Input.GetAxisRaw(_vertical);
     }
 }
